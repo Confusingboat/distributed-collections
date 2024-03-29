faeaaef1f5a90d60e025d1e0d92278943686e314
confusingboat <confusingboat@gmail.com>
2024-03-28 23:41:29 -0500

    test: add tests for AddAsync + RemoveAsync

--- tests/Distributed.Collections.Tests/DistributedHashSetTests.cs
+++ tests/Distributed.Collections.Tests/DistributedHashSetTests.cs
@@ -19,6 +19,8 @@ public abstract class IntDistributedHashSetTests<THashSet>
 
 public abstract class DistributedHashSetTests<THashSet, T> where THashSet : IDistributedHashSet<T>
 {
+    #region AddAsync Tests
+
     [Fact]
     public async Task AddAsync_should_add_item_when_it_does_not_exist()
     {
@@ -31,6 +33,53 @@ public abstract class DistributedHashSetTests<THashSet, T> where THashSet : IDis
         // Assert
         result.ShouldBeTrue();
     }
+
+    [Fact]
+    public async Task AddAsync_should_not_add_item_when_it_already_exists()
+    {
+        // Arrange
+        var hashSet = await CreateHashSet();
+        await hashSet.AddAsync(TestValues[0]);
+
+        // Act
+        var result = await hashSet.AddAsync(TestValues[0]);
+
+        // Assert
+        result.ShouldBeFalse();
+    }
+
+    #endregion
+
+    #region RemoveAsync Tests
+
+    [Fact]
+    public async Task RemoveAsync_should_remove_item_when_it_exists()
+    {
+        // Arrange
+        var hashSet = await CreateHashSet();
+        await hashSet.AddAsync(TestValues[0]);
+
+        // Act
+        var result = await hashSet.RemoveAsync(TestValues[0]);
+
+        // Assert
+        result.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task RemoveAsync_should_not_remove_item_when_it_does_not_exist()
+    {
+        // Arrange
+        var hashSet = await CreateHashSet();
+
+        // Act
+        var result = await hashSet.RemoveAsync(TestValues[0]);
+
+        // Assert
+        result.ShouldBeFalse();
+    }
+
+    #endregion
 
     protected abstract Task<THashSet> CreateHashSet();
 
